62e9cc456a54ff7a45899c542affa531fb595b7f
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2011-09-11 12:43:34 -0700

    added batch param to max-write

--- jmeter/scripts/max-write.sh
+++ jmeter/scripts/max-write.sh
@@ -3,7 +3,7 @@ RESULT_LOG="../results/create-disc-graph.log"
 HOST_UNDER_TEST="localhost"
 COUNT="100000"
 BATCH="10000"
-ENDPOINT="lab/env/max-write/${COUNT}"
+ENDPOINT="lab/env/max-write/${COUNT}/${BATCH}"
 
 echo "timed curl POST ${ENDPOINT}" >${RESULT_LOG}
 (time curl -X POST -H Accept:application/json -H Content-Type:application/json -v http://${HOST_UNDER_TEST}:7474/${ENDPOINT}) 2>${RESULT_LOG}
