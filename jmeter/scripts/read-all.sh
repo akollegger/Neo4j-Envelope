41931e0a49962ed7c4c37fc9b43ca1df3c8c4521
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2011-09-19 13:20:27 -0700

    added read-all curl script; jmeter check-all testplan

---
+++ jmeter/scripts/read-all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# operation parameters
+NODE_COUNT=${NODE_COUNT:=10000}
+
+#  build URL of the operation
+HOST_UNDER_TEST="localhost"
+HOST_PORT="7474"
+ENDPOINT="/lab/env/read-all/${NODE_COUNT}"
+OPS_URL="http://${HOST_UNDER_TEST}:${HOST_PORT}${ENDPOINT}"
+
+# logging
+SCRIPT=`basename $0`
+RESULT_LOG="../results/${SCRIPT}.log"
+JSON_RESULT="../results/${SCRIPT}.json"
+
+# run test
+echo "Script: ${SCRIPT}"  >  ${RESULT_LOG}
+echo "URL:    ${OPS_URL}" >> ${RESULT_LOG} 
+echo "" >> ${RESULT_LOG}
+
+(time curl --silent -H Accept:application/json -H Content-Type:application/json -v ${OPS_URL} > ${JSON_RESULT} ) 2>>${RESULT_LOG}
+
+echo "JSON: ${json}" >> ${RESULT_LOG}
+cat ${JSON_RESULT} >> ${RESULT_LOG}
+
